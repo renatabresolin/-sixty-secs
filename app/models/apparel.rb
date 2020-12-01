5cac30bdfe0bfdf6ac71960a3059951cae1de147
Renata Bresolin <renatagabriellib@gmail.com>
2020-12-01 11:43:06 -0300

    routes

--- app/models/apparel.rb
+++ app/models/apparel.rb
@@ -1,2 +1,4 @@
 class Apparel < ApplicationRecord
+  has_many :categories
+  has_many :tags, through: :categories
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  has_many :categories
+  has_many :recomendations
+  has_many :apparels, through: :categories
 end

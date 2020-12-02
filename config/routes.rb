1a039a33ab8b87cce08ef8139a89f101fb4affe5
Renata Bresolin <renatagabriellib@gmail.com>
2020-12-02 09:58:10 -0300

    home page para tags ser home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'tags/', to: 'tags#index'
+  root to: 'tags#index'
   get 'tags/show/', to: "tags#show_tag"
   devise_for :users
-  root to: 'pages#home'
   resources :apparels
 end

432ba040bc9a624789b15f72d36bac34bd96021f
carla kalache <carla.kalache@gmail.com>
2020-12-03 16:20:32 -0300

    style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_12_03_165446) do
-#ActiveRecord::Schema.define(version: 2020_12_03_140737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +76,7 @@ ActiveRecord::Schema.define(version: 2020_12_03_165446) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

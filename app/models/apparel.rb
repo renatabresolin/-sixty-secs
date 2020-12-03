13211d37041458692fcf5d29a4f56bff746c40cd
Renata Bresolin <renatagabriellib@gmail.com>
2020-12-03 16:17:10 -0300

    delete and edit buttons

--- app/models/apparel.rb
+++ app/models/apparel.rb
@@ -1,6 +1,6 @@
 class Apparel < ApplicationRecord
   STYLES = ['Hipster', 'Sexy', 'Clássico', 'Esportivo Casual', 'Romântico', 'Minimalista']
-  has_many :categories
+  has_many :categories, dependent: :destroy
   has_one_attached :photo
   has_many :tags, through: :categories
 end

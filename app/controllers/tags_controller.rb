1111e60726ee14fb59958bf3612c69514a3f5e9e
carla kalache <carla.kalache@gmail.com>
2020-12-04 17:11:44 -0300

    fix controller tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index ]
   def index
-    @tags = Tag.where(category: "ocasião")
+    @tags = Tag.where(category: "Ocasião")
   end
 end
